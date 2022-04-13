9d8d8a420ec33598da089e738abd0424071df9ba
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2022-04-13 10:53:17 +0100

    Added virtual environment testing

--- virtualenv_test.py
+++ virtualenv_test.py
@@ -1,3 +1,6 @@
+# Output to check current environment
+# These will match unless i'm in a venv in which case sys.prefix will be the
+# virtual environment, and base prefix will revert to global python version
 import sys
 
 print(f'sys.prefix: {sys.prefix}')
